95a5114d01630bc272f5cac931b886eb45eeeb28
Marat Mardanov <mmardano@accessholding.com>
2019-06-21 15:15:41 +0200

    initial

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN='726536102:AAF_9ERlMdPCdAFBOHfEzvpTxpOEP3njl8s'
 const bot = new TelegramBot *(TOKEN, {polling:true})
 
 bot.on ('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello from Heroku, bot says: "Hi, ${msg.from.first_name}"`)
+    bot.sendMessage(msg.chat.id,`Hello from Heroku, bot says: "Hi, ${msg.from.first_name}"`)
 }
