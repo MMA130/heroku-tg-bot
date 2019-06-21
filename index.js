0b3c15bb8fc8315b48bfc9b77226485b5ffeebfa
Marat Mardanov <mmardano@accessholding.com>
2019-06-21 15:25:34 +0200

    initial

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN='726536102:AAF_9ERlMdPCdAFBOHfEzvpTxpOEP3njl8s'
 
-const bot = new TelegramBot *(TOKEN, {polling:true})
+const bot = new TelegramBot(TOKEN, {polling:true})
 
 bot.on ('message', msg => {
     bot.sendMessage(msg.chat.id,`Hello from Heroku, bot says: "Hi, ${msg.from.first_name}"`)
